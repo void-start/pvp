e801729cceabdefde4c39cbf24d879c86ebd7f1a
void-start <81857867+void-start@users.noreply.github.com>
2025-04-29 23:22:07 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,7 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=pvp_arena', 'root', '');  // Замените данные для подключения
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Подключение не удалось: ' . $e->getMessage();
-    exit;
-}
+$pdo = new PDO('mysql:host=localhost;dbname=pvp_arena', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
+
 
 
